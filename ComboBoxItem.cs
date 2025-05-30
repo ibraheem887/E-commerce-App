502135826e255fe3fda6d0025f82ee94b48918cb
IbraheemShahid <ibraheemshahidakaibi@gmail.com>
2025-05-31 02:20:38 +0500

    Add files via upload

---
+++ ComboBoxItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class ComboBoxItem
+{
+    public string Text { get; set; }
+    public string Value { get; set; }
+
+    public override string ToString()
+    {
+        return Text; // Ensures that the `Text` property is displayed in the ComboBox
+    }
+}
